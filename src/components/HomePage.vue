88d664b39bf53791f52b4fd7b8acac7efafe3c9c
MattMoe <mrobertmoe@gmail.com>
2022-11-12 17:27:45 -0500

    refactor homepage, created navbar component

---
+++ src/components/HomePage.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+import NavBar from './NavBar.vue';
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <NavBar />
+  <div>This is in HomePage Component</div>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>This is the navbar component</h1>
+  <div class="nav">
+    <a href="#">Logo</a>
+    <a href="#">About</a>
+    <a href="#">Projects</a>
+    <a href="#">Resume</a>
+    <a href="#">Contact Me</a>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
