aa771cc816a06c5d2a149fcbc981539ce1be8338
MattMoe <mrobertmoe@gmail.com>
2022-11-19 16:48:14 -0500

    restored staged commits

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -2,5 +2,13 @@
 </script>
 
 <template>
-  <h1>About page</h1>
+  <div class="flex flex-row justify-evenly max-w-5xl h-96 items-center my-20">
+    <div class="w-full h-full">
+      Somesort of catchy art piece or canvas ? no need to center this text if canvas
+    </div>
+
+    <div class="w-full h-full border-2 border-black">
+      about me stuff
+    </div>
+  </div>
 </template>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -5,7 +5,7 @@ const introMsg = "Some sort of intro message here"
 </script>
 
 <template>
-  <div class="flex flex-row justify-evenly max-w-5xl h-96 items-center">
+  <div class="flex flex-row justify-evenly max-w-5xl h-96 items-center my-20">
     <div class="w-full h-full border-2 border-black">
       <p class="">{{ name }}</p>
       <p class="">{{ introHighlight }}</p>
