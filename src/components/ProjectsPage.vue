5778306e8ff8c72e15aadd131004fac91159ec2a
MattMoe <mrobertmoe@gmail.com>
2022-11-23 17:21:28 -0500

    rough layout of project card

--- src/components/ProjectsPage.vue
+++ src/components/ProjectsPage.vue
@@ -1,6 +1,62 @@
 <script setup>
+import ProjectsPageCardVue from './ProjectsPageCard.vue';
+const projects = [
+  {
+    id: 1,
+    cardHeader: 'some image',
+    projectName: 'My Journal',
+    projectTech: 'description of technology used',
+    projectDesc: 'summary of the project',
+  },
+  {
+    id: 2,
+    cardHeader: 'some image',
+    projectName: 'Some API project',
+    projectTech: 'description of technology used',
+    projectDesc: 'summary of the project',
+  },
+  {
+    id: 3,
+    cardHeader: 'some image',
+    projectName: 'another game like snake maybe asteroids',
+    projectTech: 'description of technology used',
+    projectDesc: 'summary of the project',
+  },
+  {
+    id: 4,
+    cardHeader: 'some image',
+    projectName: 'Cosmic Snaps',
+    projectTech: 'JavaScript, HTML, CSS, React.js, Redux, PostgreSQL, Ruby, Rails',
+    projectDesc: 'A space-themed clone of 500px, an Image Hosting Site',
+  },
+  {
+    id: 5,
+    cardHeader: 'some image',
+    projectName: 'TypeDraw',
+    projectTech: 'MongoDB, Express, React/ React Native, Node',
+    projectDesc: 'Multi-platform mobile and web application for real-time collaboration on a digital whiteboard',
+  },
+  {
+    id: 6,
+    cardHeader: 'some image',
+    projectName: 'Snake',
+    projectTech: 'JavaScript, CSS, HTML5 Canvas ',
+    projectDesc: 'Implementation of classic Snake game with extra features',
+  },
+
+]
 </script>
 
 <template>
-  <h1>Projects page</h1>
+  <div>
+    <h1>Projects page</h1>
+    <!-- 1 card for now, for loop later -->
+    <!-- gonna have to put in ul, using a for loop to return li items that return this component -->
+    <ul class="grid grid-cols-2 gap-2">
+      <li v-for="project in projects" :key="project.id" class="w-4/5 h-48">
+        <ProjectsPageCardVue :project="project" />
+      </li>
+    </ul>
+
+  </div>
 </template>
---
+++ src/components/ProjectsPageCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  project: Object
+})
+
+</script>
+
+<template>
+  <div class="card w-full h-full border border-2 text-center">
+    <div class="header">{{ project.cardHeader }}</div>
+    <div class="main-section">
+      <div class="project-title ">{{ project.projectName }}</div>
+      <div class="project-technologies">{{ project.projectTech }}</div>
+      <div class="project-description">{{ project.projectDesc }}</div>
+    </div>
+  </div>
+</template>
