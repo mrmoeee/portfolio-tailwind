17302d703c5a67568afe23268d5c70df4e6cac02
MattMoe <mrobertmoe@gmail.com>
2022-12-13 13:00:43 -0500

    Finished styling for contact form

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -15,13 +15,18 @@ const sendEmail = () => {
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <h3>Contact Me!</h3>
-    <form class="flex flex-col border border-round w-3/4">
-      <input type="text" id="name" placeholder="Your Name" required>
-      <input type="email" id="email" placeholder="Your Email" required>
-      <textarea id="message" rows="4" placeholder="Your Message"></textarea>
-      <button type="submit">Send!</button>
-    </form>
+  <p class="my-6 text-center text-2xl antialiased font-light tracking-widest">Contact Me</p>
+  <div class="flex justify-center text-base antialiased font-light tracking-widest">
+    <div class="w-5/12 border border-2 rounded p-4">
+      <form class="flex flex-col">
+        <p class="p-2">Your Name</p>
+        <input class="p-2 my-2 rounded" type="text" id="name" placeholder="Your Name" required>
+        <p class="p-2">Email Address</p>
+        <input class="p-2 my-2 rounded" type="email" id="email" placeholder="Your Email" required>
+        <p class="p-2">Message</p>
+        <textarea class="p-2 my-2 rounded" id="message" rows="4" placeholder="Your Message"></textarea>
+        <button class="p-2 my-2 border border-2 rounded" type="submit">Send!</button>
+      </form>
+    </div>
   </div>
 </template>
