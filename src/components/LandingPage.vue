01ade5d1dca576fec8bb0b97e8272e43a09bd7c8
MattMoe <mrobertmoe@gmail.com>
2022-12-16 13:09:48 -0500

    added canvas, start simple drawing

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -7,15 +7,48 @@ const introMsg = "Some sort of intro message here"
 
 onMounted(() => {
   const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
-
+  let raf;
   canvas.style.width = "100%";
   canvas.style.height = "100%";
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 
-  ctx.fillStyle = "green";
-  ctx.fillRect(canvas.width / 20, canvas.height / 20, 150, 100);
+  // check for support of canvas
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    // drawing code
+    const ball = {
+      x: 100,
+      y: 100,
+      vx: 5,
+      vy: 2,
+      radius: 25,
+      color: "blue",
+      // function to draw the object
+      draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fillStyle = this.color;
+        ctx.fill()
+      },
+    };
+
+    function draw() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ball.draw();
+      ball.x += ball.vx;
+      ball.y += ball.vy;
+      raf = window.requestAnimationFrame(draw);
+    }
+
+    // window.requestAnimationFrame(draw);
+
+    ball.draw();
+
+  } else {
+    // canvas-unsupported code here (image or art piece)
+  }
 
 })
 </script>
