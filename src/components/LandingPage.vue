2c86a29fb7ca88cb65bfdc1854fb6736d6f12c87
MattMoe <mrobertmoe@gmail.com>
2022-12-18 20:19:04 -0500

    spawned in stars, prepare for moving stars in random direction

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, onUpdated } from 'vue';
 
 const name = "Hello, my name's Matthew Moe and"
 const introHighlight = "I am a Software Developer"
@@ -17,13 +17,25 @@ onMounted(() => {
   if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
     // drawing code
-    const ball = {
-      x: 100,
-      y: 100,
-      vx: 0.2,
-      vy: 0.5,
-      radius: 25,
-      color: "blue",
+    const star = {
+      x: 0,
+      y: 0,
+      vx: 0,
+      vy: 0,
+      radius: 0,
+      color: "white",
+      // create star
+      create(x, y) {
+        let star = Object.create(this);
+        star.x = x;
+        star.y = y;
+        // set speed
+        // this.vx = this.setSpeed();
+        // this.vy = this.setSpeed();
+        // 
+        return star;
+      },
+
       // function to draw the object
       draw() {
         ctx.beginPath();
@@ -33,46 +45,68 @@ onMounted(() => {
         ctx.fill()
       },
 
-      resetBall() {
+      setSpeed(speed) {
+
+      },
+
+      update() {
+        this.x += this.vx;
+        this.y += this.vy;
+      },
+
+      resetStar() {
         this.x = 100;
         this.y = 100;
-      }
+      },
     }
     // variables for stars
     let stars = [];
     // different layers for stars
     let layers = [
-      { speed: 0.02, scale: 0.2, count: 150 },
-      { speed: 0.04, scale: 0.5, count: 50 },
-      { speed: 0.05, scale: 0.75, count: 20 },
+      { speed: 0.02, scale: 0.2, count: 500 },
+      { speed: 0.04, scale: 0.5, count: 75 },
+      { speed: 0.05, scale: 0.75, count: 30 },
     ];
-    let starBaseRadius = 3;
+    let starBaseRadius = 4;
+
+    // creating the stars
+    layers.forEach((layer) => {
+      for (let i = 0; i < layer.count; i += 1) {
+        let starLayer = star.create(randomSpawn(canvas.width), randomSpawn(canvas.height))
+        starLayer.radius = starBaseRadius * layer.scale;
+        stars.push(starLayer);
+      }
+    })
+    // function to randomize star spawn point
+    function randomSpawn(max) {
+      return Math.floor(Math.random() * max);
+    }
 
     function draw() {
+      // black background
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ball.draw();
-      ball.x += ball.vx;
-      ball.y += ball.vy;
+      ctx.fillStyle = "black";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      // draw stars
+      stars.forEach((star) => {
+        star.draw();
+        // update star here to move the star
+      })
 
-      if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
-        ball.resetBall();
+      // boundary check for stars
+      if (star.y + star.vy > canvas.height || star.y + star.vy < 0) {
+        star.resetStar();
         console.log('reset! y');
       }
-      if (ball.x + ball.xy > canvas.width || ball.x + ball.xy < 0) {
-        ball.resetBall();
+      if (star.x + star.xy > canvas.width || star.x + star.xy < 0) {
+        star.resetStar();
         console.log('reset! x');
       }
-
-
-
 
       raf = window.requestAnimationFrame(draw);
     }
 
     window.requestAnimationFrame(draw);
-
-    ball.draw();
-
   } else {
     // canvas-unsupported code here (image or art piece)
   }
@@ -88,7 +122,7 @@ onMounted(() => {
       <p class="">{{ introMsg }}</p>
     </div>
     <div id="canvas-container" class="w-full h-full">
-      <canvas class="p-2" id="canvas"></canvas>
+      <canvas class="rounded" id="canvas"></canvas>
     </div>
   </div>
 </template>
